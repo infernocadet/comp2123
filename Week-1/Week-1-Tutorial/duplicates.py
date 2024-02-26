e36461d6342022bde425a900ac5e5c4ca371944d
Marc <mcar4177@uni.sydney.edu.au>
2024-02-27 10:20:35 +1100

    week2

---
+++ Week-1/Week-1-Tutorial/duplicates.py
@@ -0,0 +1,16 @@
+def duplicates(array):
+    n = len(array)
+    seen = []
+    for i in range(0, n):
+        if array[i] in seen:
+            return True
+        seen.append(array[i])
+    return False
+
+
+ISDUP = [1, 2, 3, 4, 5, 4]
+NOTDUP = [3, 34, 1, 2, 33]
+DUPYES = [2, 2]
+print(duplicates(ISDUP))
+print(duplicates(NOTDUP))
+print(duplicates(DUPYES))
