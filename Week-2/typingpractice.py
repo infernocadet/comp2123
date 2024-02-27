813561851005adb79e0db2c750bb737f4719f687
Marc <mcar4177@uni.sydney.edu.au>
2024-02-27 15:43:29 +1100

    py

--- Week-2/typingpractice.py
+++ Week-2/typingpractice.py
@@ -33,11 +33,11 @@ customers_db = {
     2: {"name": "Bob", "age": 25, "email": "bob@example.com"},
 }
 
-def find_customer(customer_id: int) -> Optional[Dict]:
+def find_customer(customer_id: int) -> Optional[Dict[str, str]]:
     """Fetch cx info using customer ID.
     Args: customer_id (int)
     Returns:
-    Optional[Dict{str, str}]: Cx info, if found, if nothing, then None
+    Optional[Dict[str, str]]: Cx info, if found, if nothing, then None
     """
 
     return customers_db.get(customer_id, None)
@@ -46,6 +46,28 @@ def process_data(data: Union[str, List[str]]) -> None:
     # Function can handle both a string, or a list of strings
     pass
 
+print(find_customer(0))
+print(find_customer(1))
 
-find_customer(0)
-find_customer(1)
+# 4. Get Familiar with Generics
+
+from typing import TypeVar, Generic, List
+
+T = TypeVar('T')
+
+class Stack(Generic[T]):
+    def __init__(self) -> None:
+        self._container: List[T] = []
+
+    def push(self, item: T) -> None:
+        self._container.append(item)
+
+    def pop(self) -> T:
+        return self._container.pop()
+
+'''
+TypeVar is a function used to define a generic type variable.
+Allows us to write functions, methods, and classes which can operate
+on any type, with the specific type being determined
+when the function is called or class is instantiated.
+'''
