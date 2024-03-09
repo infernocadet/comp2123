504ab85f77a8d79a6d93cc2f24b1368dd6349758
Marc <caramoanmarc@gmail.com>
2024-03-10 10:40:29 +1100

    treenode

--- Week-3/treenode.py
+++ Week-3/treenode.py
@@ -12,7 +12,7 @@ Each node contains a value, a pointer to its parent and a list of children.
 class Node(Generic[T]):
 
     _value: T
-    _parent: 'Node(T)'
+    _parent: 'Node[T]'
     _children: List['Node[T]']
     _subtree_size: int
 
@@ -75,3 +75,45 @@ class Node(Generic[T]):
         Set subtree size to specified node.
         :param value: value to set the size to
         """
+        self._subtree_size = value
+    
+    def add_child(self, child: 'Node[T]') -> None:
+        """
+        Adds child to the children list of the current node.
+        :param child: child node to add to list.
+        """
+        self._children.append(child)
+    
+    def get_children(self) -> List['Node[T]']:
+        """
+        Returns the children of the node.
+        :return: The list of children belonging to the current node.
+        """
+        return self._children
+    
+    def num_children(self) -> int:
+        """
+        :return: The number of direct children belonging to the current node.
+        """
+        return len(self._children)
+    
+    def is_internal(p: 'Node[T]') -> bool:
+        """
+        Returns true if node is internal and false otherwise
+        :param p: the given node
+        """
+        return (len(p._children) > 0)
+    
+    def is_external(p: 'Node[T]') -> bool:
+        """
+        Returns True if node is external and false otherwise
+        :param p: The given node
+        """
+        return (len(p._children) == 0)
+    
+    def is_root(p: 'Node[T]') -> bool:
+        """
+        Returns True if node is root and false otherwise
+        :param p: The given node
+        """
+        return (p._parent == None)
